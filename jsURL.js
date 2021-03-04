3c4ffda999bf5b32d6d748a026810cbcaabd1e8c
Limnuz <marceloln82@yahoo.com.br>
2021-03-04 11:06:36 -0300

    implementado parâmetro separador, tratada situação de url sem parâmetro, adicionados alguns atributos ao objeto final

--- jsURL.js
+++ jsURL.js
@@ -1,22 +1,35 @@
 //url = document.URL
-var url = 'https://limnuz.github.io/index.html?channel="https://www.youtube.com/channel/UCwJchClzCtxlv8QFGH6pjRQ?sub_confirmation=1"&link=http://duckduckgo.com'
+var url = 'https://limnuz.github.io/index.html?channel=##https://www.youtube.com/channel/UCwJchClzCtxlv8QFGH6pjRQ?sub_confirmation=1##&link=http://duckduckgo.com'
+//var url = 'https://limnuz.github.io/index.html?channel=##https://www.youtube.com/channel/UCwJchClzCtxlv8QFGH6pjRQ?sub_confirmation=1##'
 
-function jsURL(url){
-    //Separa o conteúdo entre aspas(convenciona-se ser endereços web com "?", "&" e "=") e os codifica em base-64 para não misturar com o resto da string
-    url = url.split('"')
+function jsURL(url, separator='"'){
+    //separator: separador usado na URL para separar parâmetros que possam conter os caracteres "?", "&" e "="), por padão são aspas duplas, mas pode ser substituido por outros simbolos conforme necessidade
+    
+    if(url.indexOf("?") == -1){
+        return {urlPage:url, purePage:url, containsParameter:false, infoString:`"urlPage":"url", "purePage":"url"`}
+    }
+    
+    var urlPage = url
+    url = url.split(separator)
+    
+    //codifica os atributos entre os separadores("")
     for(i in url){
         if(i%2 != 0){
             
-            url[i] = '"' + window.btoa(`${url[i]}`) + '"'
+            url[i] = separator + window.btoa(url[i]) + separator
+            
         }
     }
-
-    //Reune novamente a string depois de removidas as aspas("") e codificados os endereços
+    
+    
+    //Reune novamente a string depois de codificados os endereços
     var temp = ""
     for(let i in url){
         temp += url[i]
     }
 
+    
+    var purePage = temp.split("?")[0]
     url = temp.split("?")[1]
     
     
@@ -34,17 +47,24 @@ function jsURL(url){
     //deixa os parametros em formato de texto para ser convertido em JSON
     temp = "{"
     for(let i = 0; i < url.length; i+=2){
-        if(url[i+1].indexOf('"') == -1){
-            temp += `"${url[i]}":"${url[i+1]}",'`
+        if(url[i+1].indexOf(separator) == -1){
+            temp += `"${url[i]}":"${url[i+1]}",`
         } else {
-            window.alert(i)
-            temp += `"${url[i]}":"${window.atob(url[i+1].split('"')[1])}",`
+            
+            temp += `"${url[i]}":"${window.atob(url[i+1].split(separator)[1])}",`
         }
     }
-    url = temp.substring(0, temp.length - 2) + "}"
-    window.alert(url)
+    
+    temp = temp.substring(0, temp.length - 1) + "}"
+    
+    url = temp
 
     url = JSON.parse(url)
+    url.urlPage = urlPage
+    url.purePage = purePage
+    url.infoString = temp
+    url.containsParameters = true
+    
 
     return url
 }
@@ -52,11 +72,7 @@ function jsURL(url){
 function teste1(){
     var body = document.body
 
-    url = jsURL(url)
+    url = jsURL(url, "##")
 
-    body.innerHTML = `${url.channel}, ${url.link}`
+    body.innerHTML = `${url.channel}<br> ${url.link} <br> ${url.infoString} <br> ${url.purePage} <br> ${url.urlPage}`
 }
-
-
-
-//https://limnuz.github.io/index.html?channel="https://www.youtube.com/channel/UCwJchClzCtxlv8QFGH6pjRQ?sub_confirmation=1"&link=http://duckduckgo.com
